209820a106d8a2a3f546c5e9b555e00fd3755038
old4ever <git@old4ever.simplelogin.com>
2023-11-22 18:05:48 +0200

    fixed middleware requiring auth for view of public tweets (still don't know why it blocks post view and user profile)

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,15 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  beforeAuth: () => {
-    console.log("This is a middleware log");
-  },
+  // beforeAuth: () => {
+  //   console.log("This is a middleware log");
+  // },
+  publicRoutes: [
+    "/api/trpc/posts.getAll",
+    // "/api/trpc/posts.getById",
+    // "/api/trpc/posts.getPostsByUserId",
+    // "/api/trpc/profile.getUserByUsername",
+  ],
 });
 
 export const config = {
